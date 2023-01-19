22be5d6a486618277aafcba34e295685905c54c6
andreeamaria1568 <tipaldoalexis@yahoo.com>
2023-01-19 04:09:36 +0200

    Adaugare comentarii pe o singura linie

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -1,7 +1,10 @@
 package ro.java.ctrln;
 
 public class Main {
+
+    //Aceasta este metoda Main, care porneste o aplicatie Java
     public static void main(String[] args) {
+        //Aici vrem sa afisam pe consola Intelij un mesaj ales de noi
         System.out.println("Invat limbajul Java si despre masina virtuala Java!");
     }
 }
